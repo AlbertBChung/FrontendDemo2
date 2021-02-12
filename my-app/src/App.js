eeddc34c58557a2edd3d1a32c2ae3c4183d31692
Albert Chung <albertc435@gmail.com>
2021-02-12 11:38:04 -0500

    initial push

---
+++ my-app/src/App.js
@@ -0,0 +1,66 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/instagram/edsheerantopfivephotourls")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.photos
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const error = this.state.error;
+    const isLoaded = this.state.isLoaded;
+    const items = this.state.items;
+
+    console.log(items)
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+      <div>
+
+        My favorite Images
+        <ul>
+          {items.map(item => (
+            <li key={item}>
+              <img src={item} width="500" height="600" />
+            </li>
+          ))}
+        </ul>
+      </div>
+      );
+    }
+  }
+}
+
+export default App;
